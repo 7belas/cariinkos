1eccdd2eeb606a75227ec7ee003ee089cf51c651
fendy <fmarzuki20@gmail.com>
2019-09-25 21:20:06 +0700

    aaaa

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title','Selamat datang Admin')
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/172410101106', 'AdminController@index')->name('home');
+
 Route::get('/kirimemail','KosEmailController@index');
 
 Auth::routes();
