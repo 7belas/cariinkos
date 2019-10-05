d5ecbd1b5e7256100a713af3fdca2de2f11fd8e1
fendy <fmarzuki20@gmail.com>
2019-10-05 19:59:00 +0700

    Update isisaldo.blade.php

--- resources/views/isisaldo.blade.php
+++ resources/views/isisaldo.blade.php
@@ -9,4 +9,32 @@
 
                 <div class="card-body">
 
+                <table class="table">
+                
+                    <tbody>
+                        <tr>
+                        
+                        <tr>
+                        <th>Pilih Bank</th>
+                            <td> <div class="radio">
+                            <label><input type="radio" name="optradio" checked>Bank BRI</label>
+                            </div></td>
+                            
+                            <th scope ="row"> <td> <div class="radio">
+                            <label><input type="radio" name="optradio" checked>Bank BNI</label>
+                            </div></td></th>   
+
+                            <td> <div class="radio">
+                            <label><input type="radio" name="optradio" checked>Bank BCA</label>
+                            </div></td>
+
+                            <td> <div class="radio">
+                            <label><input type="radio" name="optradio" checked>Bank Mandiri</label>
+                            </div></td>
+
+
+                        </tr>
+                        <tr>
+
+
 @endsection
